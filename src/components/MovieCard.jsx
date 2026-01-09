b03c715f2315d8cdb1ae87aef25c157a386113fa
MohamedSamir11113 <simo2000854@gmail.com>
2026-01-10 00:04:08 +0200

    added movie votes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,6 +6,7 @@ export const MovieCard = ({movie:{title,vote_average,poster_path,release_date,or
     <div className='movie-card'>
         <img src={poster_path?`https://image.tmdb.org/t/p/w500/${poster_path}`:'/no-movie.png'} alt="" />
         <h3>{title}</h3>
+        <p>{vote_average}</p>
         </div>
   )
 }
