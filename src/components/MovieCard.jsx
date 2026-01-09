182835ff400b68fb84fe8a47908a8785797a22df
MohamedSamir11113 <simo2000854@gmail.com>
2026-01-10 01:47:19 +0200

    hasbik added name

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
 export const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_language}}) => {
-console.log("dev-jsm is modyfying here");
-console.log("yo hi there");
-
-  console.log("this is coming from dev-adrian");
+  console.log("this is coming from hasbik");
   
   return (
     <div className='movie-card'>
         <img src={poster_path?`https://image.tmdb.org/t/p/w500/${poster_path}`:'/no-movie.png'} alt="" />
-        <h3>{title}</h3>
+        <h3>{`${title} hasbik`}</h3>
         <p>{vote_average}</p>
         <p>{release_date}</p>
       
