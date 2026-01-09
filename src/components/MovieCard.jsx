9116f9c8df63ab184c4e5d2c5d4845442b6af930
MohamedSamir11113 <simo2000854@gmail.com>
2026-01-10 01:20:00 +0200

    modifying Card by adding the date and a comment

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 export const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_language}}) => {
 
+  console.log("this is coming from dev-adrian");
+  
   return (
     <div className='movie-card'>
         <img src={poster_path?`https://image.tmdb.org/t/p/w500/${poster_path}`:'/no-movie.png'} alt="" />
         <h3>{title}</h3>
         <p>{vote_average}</p>
+        <p>{release_date}</p>
       
         </div>
   )
