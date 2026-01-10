693a5e22078b8592a9365185ef93c0c03d5a5654
MohamedSamir11113 <simo2000854@gmail.com>
2026-01-10 02:29:15 +0200

    changed title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ export const MovieCard = ({movie:{title,vote_average,poster_path,release_date,or
   return (
     <div className='movie-card'>
         <img src={poster_path?`https://image.tmdb.org/t/p/w500/${poster_path}`:'/no-movie.png'} alt="" />
-        <h3>{`${title} simo11`}</h3>
+        <h3>{`${title} simo11111`}</h3>
         <p>{vote_average}</p>
         <p>{release_date}</p>
         <p>{original_language}</p>
