dd3349ca8eead6b931a6c28f0bdacd643cfa0b32
MohamedSamir11113 <simo2000854@gmail.com>
2026-01-10 02:36:21 +0200

    add bombo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 export const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_language}}) => {
   console.log("this is coming from hasbik");
+  console.log("this is coming from ashalee");
+  console.log("this is coming from bombo");
+  console.log("this is coming from eeew");
   
   return (
     <div className='movie-card'>
         <img src={poster_path?`https://image.tmdb.org/t/p/w500/${poster_path}`:'/no-movie.png'} alt="" />
         <h3>{`${title} hasbik`}</h3>
-        <p>{vote_average}</p>
+        <p>{vote_average} ashalee</p>
         <p>{release_date}</p>
       
         </div>
